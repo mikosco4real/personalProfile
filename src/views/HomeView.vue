4f2d7f3c41f7e926715cd0b6dfe0222e5c616b1d
Michael Okolo <okolomikel2009@gmail.com>
2024-10-07 17:44:14 +1100

    Updated Home page testing delivery pipeline

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="p-8">
     <h1 class="text-4xl font-bold mb-4">Home</h1>
     <p>Welcome to my personal website! My name is Michael Okolo and it is an honor to meet you.</p>
-    <p> More updates coming soon </p>
+    <p> More updates coming soon, With delivery pipeline already in place </p>
   </div>
 </template>
 
