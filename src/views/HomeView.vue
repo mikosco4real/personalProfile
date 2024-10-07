c04f53d34de586f66d701da9cc9dfb9a8e8a9659
Michael Okolo <okolomikel2009@gmail.com>
2024-10-07 16:14:38 +1100

    Updated Home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="p-8">
     <h1 class="text-4xl font-bold mb-4">Home</h1>
     <p>Welcome to my personal website! My name is Michael Okolo and it is an honor to meet you.</p>
+    <p> More updates coming soon </p>
   </div>
 </template>
 
