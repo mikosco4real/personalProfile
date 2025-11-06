9299b1f8a5015311547b353e0faff72cc0281224
Michael Okolo <okolomikel2009@gmail.com>
2025-11-06 23:15:09 +1100

    Added a new line to test my pipeline

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="p-8">
     <h1 class="text-4xl font-bold mb-4">Home</h1>
-    <p>Welcome to my personal website! My name is Michael Okolo and it is an honor to meet you.</p>
-    <p> More updates coming soon, With delivery pipeline already in place </p>
+    <p>
+      Welcome to my personal website! My name is Michael Okolo and it is an
+      honor to meet you.
+    </p>
+    <p>We are now in the world of AI. Expect new AI updates soon.</p>
+    <p>More updates coming soon, With delivery pipeline already in place</p>
   </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
 /* Add any scoped styles here */
